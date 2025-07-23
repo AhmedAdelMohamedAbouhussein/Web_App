423c43191e3fda1ad6553316b038e0ae4cf0a326
Ahmed Adel <aa5913372@gmail.com>
2025-07-23 19:50:46 +0300

    fix attempt #1

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,8 @@
+// my-react-app/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Web_App/'
 })
