c370dd8424233751bc5025bbdd60e2c79ace7675
Ahmed Adel <aa5913372@gmail.com>
2025-07-24 21:45:34 +0300

    install Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import Footer from "./Conponents/Footer.jsx";
 import  Header from "./Conponents/Header.jsx" //when exporting defailt, dont use curly braces 
+import LoginPage from "./Conponents/LoginPage.jsx";
 
 function App() 
 {
     return(
         <>
-            <Header />
-            <Footer />
+            <LoginPage />
         </>
-        
     );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 // my-react-app/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   base: '/Web_App/'
 })
