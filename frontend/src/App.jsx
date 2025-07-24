e2fa3509f5ef76dc1f2f6224bddba3239f3d374e
Ahmed Adel <aa5913372@gmail.com>
2025-07-24 14:56:26 +0300

    new components + rename reactapp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Conponents/Footer.jsx";
+import  Header from "./Conponents/Header.jsx" //when exporting defailt, dont use curly braces 
+
+function App() 
+{
+    return(
+        <>
+            <Header />
+            <Footer />
+        </>
+        
+    );
+}
+
+export default App
---
+++ frontend/src/Conponents/Footer.jsx
@@ -0,0 +1,11 @@
+//jsx :- JavaScript XML
+
+function Footer() 
+{
+    return(
+        <footer>
+            <p> &copy; {new Date().getFullYear()} My React App</p>
+        </footer>
+    );
+}
+export default Footer;
